9b2d4a6607ae236eaef2be9e03725b2bd8affa4f
RhuanLino <rhuanlino309@gmail.com>
2024-07-23 12:34:49 -0300

    fetch: add operadores sintax

--- java-basico/src/Operadores.java
+++ java-basico/src/Operadores.java
@@ -1,5 +1,27 @@
 public class Operadores {
     public static void main(String [] args) {
         
+        //Adição e concatenar
+        int soma = 2 + 4;
+        String nome = "Rhuan" + "Lino";
+
+        //Subtração
+        int subtração = 10 - 5;
+
+        //Multiplicação
+        int multiplicação = 2 * 5;
+
+        //Divisão
+        int divisao = 10 / 2;
+
+        //Incrementar usa-se ++
+        //Decrementar usa-se --
+
+        //Operadores de comparação
+        
+        //Maior igual: >=
+        //Menor igual: <=
+        //Igual: ==
+        //Diferente: !=
     }
 }
