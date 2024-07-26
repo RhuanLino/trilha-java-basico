18d173defde43f5cad44bc6cfe27f0063a4feb20
RhuanLino <rhuanlino309@gmail.com>
2024-07-26 12:17:24 -0300

    feat: add docs examples

---
+++ java-docs/README.md
@@ -0,0 +1,33 @@
+# Java-Docs
+
+Arquivo .java com exemplo e modelo de para fazer documentação de códigos.
+
+#### Comentário em linha
+
+```java
+  //Este é um comentário em linha.
+```
+
+#### Comentário em múltiplas linhas
+
+```java
+  /*
+  * Este é um exemplo
+  * de comentários em
+  * múltiplas linhas.
+  */
+```
+
+#### Comentário para documentação
+
+```java
+  /**
+  * Este é um exemplo
+  * de docs que pode
+  * ser usado para documentar
+  * códigos.
+  * @author Rhuan Lino
+  * @version 1.0
+  * @since 01/01/01
+  */
+```
---
+++ java-docs/src/Docs.java
@@ -0,0 +1,23 @@
+
+    /**
+     * Este é um exemplo de docs
+     * que pode ser usado para 
+     * documentar códigos.
+     * @author Rhuan Lino
+     * @version 1.0
+     * @since 26/07/2024
+     */
+public class Docs {
+    
+    /**
+     * Este é uma função que soma dois valores inteiros
+     * e retorna um resultado inteiro.
+     * @param valor1 - primeiro valor
+     * @param valor2 - segundo valor
+     * @return - resultado inteiro
+     */
+
+    public int soma (int valor1, int valor2) {
+        return valor1 + valor2;
+    }
+}
