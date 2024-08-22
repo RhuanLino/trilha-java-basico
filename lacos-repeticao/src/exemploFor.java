e1a19482368e2900cde4c2ce1864a2df35a05c99
RhuanLino <rhuanlino309@gmail.com>
2024-08-22 10:57:04 -0300

    fetch: add documentation in (.lacos-repeticao/src/exemploFor.java)

--- lacos-repeticao/src/exemploFor.java
+++ lacos-repeticao/src/exemploFor.java
@@ -3,6 +3,8 @@ public class exemploFor {
         // Definindo um array com o nome de três alunos
         String alunos [] = {"Ruan", "Marcos", "João"};
 
+        // Este método "for" percorre a lista [alunos] e enquanto a variável [x] for menor que o tamanho da lista [alunos.length],
+        // o método acrescentará +1 a cada iteração
         for (int x = 0; x < alunos.length; x++) {
             System.out.println("O aluno de índice " + x + " é " + alunos[x]);
         }
